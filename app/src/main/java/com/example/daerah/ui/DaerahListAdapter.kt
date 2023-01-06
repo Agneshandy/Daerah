dd95d27f8901df16c5ab5f27f166dce526445ba5
Agnes Belladona Shandy Ayocha <agneshandy21@gmail.com>
2023-01-06 09:03:12 +0700

    menambahkan file daerah.apk

---
+++ app/src/main/java/com/example/daerah/ui/DaerahListAdapter.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.daerah.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.daerah.databinding.ListViewItemBinding
+import com.example.daerah.network.Daerah
+
+/**
+ * This class implements a [RecyclerView] [ListAdapter] which uses Data Binding to present [List]
+ * data, including computing diffs between lists.
+ */
+class DaerahListAdapter() :
+    ListAdapter<Daerah, DaerahListAdapter.DaerahViewHolder>(DiffCallback) {
+
+    class DaerahViewHolder(
+        var binding: ListViewItemBinding
+        ) : RecyclerView.ViewHolder(binding.root){
+        fun bind(daerah: Daerah) {
+            binding.daerah = daerah
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Daerah>() {
+
+        override fun areItemsTheSame(oldItem: Daerah, newItem: Daerah): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Daerah, newItem: Daerah): Boolean {
+            return oldItem.nama == newItem.nama
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DaerahViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return DaerahViewHolder(
+            ListViewItemBinding.inflate(layoutInflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: DaerahViewHolder, position: Int) {
+        val daerah = getItem(position)
+        holder.bind(daerah)
+    }
+}
+
+class DaerahListener(val clickListener: (daerah: Daerah) -> Unit) {
+    fun onClick(daerah: Daerah) = clickListener(daerah)
+}
---
+++ app/src/main/java/com/example/daerah/ui/DaerahViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.daerah.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.daerah.network.Daerah
+import com.example.daerah.network.DaerahApi
+import kotlinx.coroutines.launch
+
+enum class DaerahApiStatus { LOADING, ERROR, DONE }
+
+class DaerahViewModel : ViewModel() {
+    private val _status = MutableLiveData<DaerahApiStatus>()
+    val status: LiveData<DaerahApiStatus> = _status
+
+    private val _daerahs = MutableLiveData<List<Daerah>>()
+    val daerahs: LiveData<List<Daerah>> = _daerahs
+
+    private val _daerah = MutableLiveData<Daerah>()
+    val daerah: LiveData<Daerah> = _daerah
+
+    fun getDaerahList() {
+        _status.value = DaerahApiStatus.LOADING
+        viewModelScope.launch {
+            try {
+                _daerahs.value = DaerahApi.service.getDaerahs()
+                _status.value = DaerahApiStatus.DONE
+            } catch (e: Exception) {
+                _daerahs.value = emptyList()
+                _status.value = DaerahApiStatus.ERROR
+            }
+        }
+    }
+
+    fun onDaerahClicked(daerah: Daerah) {
+        _daerah.value = daerah
+    }
+}
